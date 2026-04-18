f87389114f7da407f3d723c974650e56fb3edbbc
vivekkumarbiswal <vivekkumarbiswal@gmail.com>
2026-04-19 00:03:55 +0530

    configured route

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { CatnavigationComponent } from './components/catnavigation/catnavigation.component';
-import { HeaderComponent } from './components/header/header.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, CatnavigationComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })

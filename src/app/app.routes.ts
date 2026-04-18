d29228607bd33032292f259cabe85e70efe4bccf
vivekkumarbiswal <vivekkumarbiswal@gmail.com>
2026-04-19 00:25:02 +0530

    Page not found 404 done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 export const routes: Routes = [
   { path: 'home', component: HomeComponent },
@@ -7,5 +8,9 @@ export const routes: Routes = [
     path: '',
     redirectTo: '/home',
     pathMatch: 'full',
+  },
+  {
+    path: '**',
+    component: NotFoundComponent,
   },
 ];
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { CatnavigationComponent } from './catnavigation/catnavigation.component';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [HeaderComponent, CatnavigationComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}

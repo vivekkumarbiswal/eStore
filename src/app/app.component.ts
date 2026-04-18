84bb232ebf0ecc3b7752142ac4008b1b9f79418c
vivekkumarbiswal <vivekkumarbiswal@gmail.com>
2026-04-18 23:53:15 +0530

    category navigation added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { CatnavigationComponent } from './components/catnavigation/catnavigation.component';
 import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, CatnavigationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
---
+++ src/app/components/catnavigation/catnavigation.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-catnavigation',
+  imports: [],
+  templateUrl: './catnavigation.component.html',
+  styleUrl: './catnavigation.component.css'
+})
+export class CatnavigationComponent {
+
+}

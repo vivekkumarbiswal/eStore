0cd615c3a2f63137e54ef17d025ab58ad1f78184
vivekkumarbiswal <vivekkumarbiswal@gmail.com>
2026-04-22 00:55:38 +0530

    sidenavigation added

---
+++ src/app/components/home/sidenavigation/sidenavigation.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sidenavigation',
+  imports: [],
+  templateUrl: './sidenavigation.component.html',
+  styleUrl: './sidenavigation.component.css'
+})
+export class SidenavigationComponent {
+
+}
